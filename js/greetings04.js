b788377887fb63c03f4827d875c6b6e854996993
KimByungwok <quddnrquddnr2302@gmail.com>
2022-07-03 16:21:28 +0900

    22.07.03
    노마더 04~05 공부

--- js/greetings04.js
+++ js/greetings04.js
@@ -4,18 +4,37 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 // const loginButton = document.querySelector("#login-form button");
 
-const hidden_ClassName = "hidden";
+const HIDDEN_CLASS = "hidden";
+const USERNAME_KEY = "username";
 
 const link = document.querySelector("a");
 
 function onLoginSubmit(event) {
     event.preventDefault(); // 브라우저 기본 동작을 막는 것
+    loginForm.classList.add(HIDDEN_CLASS);
     const username = loginInput.value;
-    loginForm.classList.add(hidden_ClassName);
-    localStorage.setItem("username", username);
+    localStorage.setItem(USERNAME_KEY, username);
     // greeting.innerText = "Hello " + username;  // 이거나 밑에거 사용하면 되는데 밑에거가 깔끔함
-    greeting.innerText = `Hello ${username}`;  // ' , " 아니고 백틱 ` 물결임 이거하면 바로 출력가능
-    greeting.classList.remove(hidden_ClassName);
+    //greeting.innerText = `Hello ${username}`;  // ' , " 아니고 백틱 ` 물결임 이거하면 바로 출력가능
+    //greeting.classList.remove(HIDDEN_CLASS);
+    painGreeting(username);
+}
+    loginForm.addEventListener('submit', onLoginSubmit);
+
+    const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+    
+    if(savedUsername === null){
+        // show the form
+        loginForm.classList.remove(HIDDEN_CLASS);
+        loginForm.addEventListener("submit",onLoginSubmit);
+    }else{
+        //show the greeting
+        //greeting.innerText = `Hello ${savedUsername}`;
+        //greeting.classList.remove(HIDDEN_CLASS);
+        painGreeting(savedUsername);
+    }
+
 
     // console.log(username);
 
@@ -28,6 +47,10 @@ function onLoginSubmit(event) {
 
     // console.log("hello", loginInput.value);
     // console.log("click");
+
+function painGreeting(username){
+    greeting.innerText = `Hello ${savedUsername}`;
+    greeting.classList.remove(HIDDEN_CLASS);
 }
 
 function handleLinkClick(event) {
